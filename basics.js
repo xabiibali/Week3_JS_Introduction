6f09b855d92ebd9af0366f3c166df5ee663d67f1
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2023-02-19 10:29:02 -0600

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,12 +1,12 @@
 /*
-Task: Convert Strings to Numbers
+Task: Convert Strings to Numbers / String number ku badal
 
 Do the following:   
-   1. Declare a string type variable with the value of "1999"
+   1. Declare a string type variable with the value of "1999" 
    2. Convert the string value of "1999" to a integer value of 1999
    3. Return the result
 
-   HINT: look up the Number method
+   HINT: look up the Number method / Raadi Number Method wax la dhaho
 */
 
 
@@ -14,23 +14,28 @@ Do the following:
 Task: Mood Checker
 
 Do the following:   
-   1. Write a script that prompts the user for their current mood. 
-   2. If the user inputs happy, print 'Yay me too!' to the console, sad print 'Aw cheer up',
-   3. Else just print 'So moody!'
+   1. Write a script that prompts the user for their current mood. / Code qor u soo saaraayo qof meeshuu ku qori lahaa siduu dareemaayo
+   2. If the user inputs happy, print 'Yay me too!' to the console, sad print 'Aw cheer up', / Haduu qofka galiyo 'happy', console log samee ah 'Yay me too', haduu 'sad' galiyo, 'Aw Cheer up' console ku qor.
+   3. Else just print 'So moody!' / Hadii kale 'So Moody!' soo saar.
 
 */
 
 
 /*
-Task: Odd or Even
+Task: Odd or Even / kisi ama dhaban
 
 Use conditionals to check if a hardcoded number is odd or even, and then console.log the number is odd or even with the numbers value.
 
+Adigoo 'if/else' isticmaalaayo hubi in nambar uu yahay 'kisi ama dhaban', kadi console log ku samee adigoo sheegaayo midkuu yahay
+
 */
 
-var num = 16; // You can change this number!
+var num = 16; // You can change this number! / Number-kaan ku bilow
 
-// write your conditions here
+// write your conditions here / Code-kaada halkaan ku qor
+
+
+
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task: FIZZBUZZ 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -38,31 +43,44 @@ var num = 16; // You can change this number!
 /*
 
 You're about to do an assignment called "Fizz Buzz", which is one of the classic programming challenges. 
-It is a favorite for interviewers, and a shocking number of job-applicants can't get it right. 
-But you won't be one of those people. Here are the rules for the assignment:
+
+"Fizz Buzz" waa xujo la siiyo markii dadka interview laga qaadaayo. Waa inuu programkaada soo saaro "Fizz", "Buzz" ama "FizzBuzz". 
+
 
 Write a program that prints the numbers from 1 to 100.
 
+Code qor soo saaraayo inta u dhaxeysa 1 ilaa 100.
+
 But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+
+Markii numberka lagu dhufan karo 3, waa inuu soo qoraa "Fizz", haduu numberka lagu dhufan karo 5, waa inuu soo qoraa "Buzz"
+
 
 For numbers which are multiples of both three and five print "FizzBuzz".
 
-The one hint you'll likely need is:
+Hadii labadaba (3 iyo 5) lagu dhufan karo, waa inuu soo qoraa "FizzBuzz".
+
+The one hint you'll likely need is / Halka wax ood u baahantahay inaa ogaato:
 
 There is a Javascript operator called "remainder" but often referred to as "modulus" or "modulo". 
 It is represented by the percentage sign (%) and it gives you the remainder left over after division. So for example:
+
+Waxaa jiro wax la yiraahdo "modulus" oo Javascript ku jiro, kaas oo uu kuu soo saaraayo haraaga labo number oo la isku dhuftay, sidaan ayuu u qoranyahay:
 
  6 % 3
 
 Equals zero. Because after dividing 6 by 3, there is zero leftover. Whereas:
 
+Waa ebar, hadii lix loo qaybiyo sadex, waxbo ma soo haraayaan.
+
 6 % 4
 
 Equals 2. Because after dividing 6 by 4, there are 2 left over from the six.
 
-If that was confusing, don't worry. It will make more sense as you use it. 
-The point is: the remainder operator is useful for finding out if X is a multiple of Y. If it is, then X % Y will yield zero. 
-Knowing this should help you complete this assignment without any issue.
+Waa labo, haduu 6 loo qaybiyo 4, lao ayaa soo haraayo.
+
+Knowing this should help you complete this assignment without any issue. Xarafkaa "%" wuxuu kaa cawinayaa inaa haraaga hesho, asaga u isticmaal inaa
+ogaato in number ay isku qaybsamaan.
 
 
 Extra Credit:
